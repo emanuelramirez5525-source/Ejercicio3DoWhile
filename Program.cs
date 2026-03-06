dc9e235b2af738a4a126c9161c09de358ccfb586
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 18:11:05 -0600

    Logica Aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿// Cree un programa que imprima los números del 10 al 1 en orden descendente utilizando do-while.
+
+int num = 10;
+
+do
+{
+    Console.WriteLine(num);
+    num--;
+}  while (num >=1);
